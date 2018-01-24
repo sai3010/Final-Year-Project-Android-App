296e7fef7efd9e962c9249cbd2368c53128cc219
sai3010 <saiprasanth2007@gmail.com>
2018-01-24 22:27:12 +0530

    added studentpage

--- app/src/main/java/com/example/saipr/final_year_proj/LoginMainActivity.java
+++ app/src/main/java/com/example/saipr/final_year_proj/LoginMainActivity.java
@@ -1,5 +1,6 @@
 package com.example.saipr.final_year_proj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class LoginMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+        /*//Toast.makeText(MainActivity.this, ":"+name+"\n:"+email+"\n:"+mob+"\n:"+address+"\n:"+pass+"\n:"+cpass, Toast.LENGTH_SHORT).show();
+
+
+        Intent i= new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(i);*/
+
     }
 }
