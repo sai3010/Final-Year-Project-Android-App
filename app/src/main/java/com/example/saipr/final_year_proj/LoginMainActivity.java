5cf82de92c38d8b032a5e370a84ce82d585d58ab
sai3010 <saiprasanth2007@gmail.com>
2018-01-25 14:14:47 +0530

    modified a bit

--- app/src/main/java/com/example/saipr/final_year_proj/LoginMainActivity.java
+++ app/src/main/java/com/example/saipr/final_year_proj/LoginMainActivity.java
@@ -3,6 +3,8 @@ package com.example.saipr.final_year_proj;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginMainActivity extends AppCompatActivity {
 
@@ -10,11 +12,15 @@ public class LoginMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
-        /*//Toast.makeText(MainActivity.this, ":"+name+"\n:"+email+"\n:"+mob+"\n:"+address+"\n:"+pass+"\n:"+cpass, Toast.LENGTH_SHORT).show();
-
-
-        Intent i= new Intent(MainActivity.this,LoginActivity.class);
-        startActivity(i);*/
-
+        Button std;
+        std = findViewById(R.id.stdlogin);
+        std.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginMainActivity.this, StudentLoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
+        /*//Toast.makeText(MainActivity.this, ":"+name+"\n:"+email+"\n:"+mob+"\n:"+address+"\n:"+pass+"\n:"+cpass, Toast.LENGTH_SHORT).show();*/
