79897446bb52c47cd421571f629e0aaffb363e1c
sai3010 <saiprasanth2007@gmail.com>
2018-01-25 13:34:22 +0530

    added new student login

--- app/src/main/java/com/example/saipr/final_year_proj/StudentLoginActivity.java
+++ app/src/main/java/com/example/saipr/final_year_proj/StudentLoginActivity.java
@@ -3,11 +3,11 @@ package com.example.saipr.final_year_proj;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StudentActivity extends AppCompatActivity {
+public class StudentLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student);
+        setContentView(R.layout.activity_student_login);
     }
 }
