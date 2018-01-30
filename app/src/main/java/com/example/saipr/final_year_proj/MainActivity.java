26e102c17a3e6e54d269514bf5cce706f336d096
sai3010 <saiprasanth2007@gmail.com>
2018-01-30 12:08:56 +0530

    updated main_Activity,login and reg_main activities

--- app/src/main/java/com/example/saipr/final_year_proj/MainActivity.java
+++ app/src/main/java/com/example/saipr/final_year_proj/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
