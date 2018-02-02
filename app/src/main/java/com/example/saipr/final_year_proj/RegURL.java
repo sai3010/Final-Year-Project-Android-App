bebfdc88adced82942628ec7be1734ee39be020c
sai3010 <saiprasanth2007@gmail.com>
2018-02-02 21:45:20 +0530

    inital jsp servelet commit

--- app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
+++
@@ -1,9 +0,0 @@
-package com.example.saipr.final_year_proj;
-
-/**
- * Created by saipr on 2/2/2018.
- */
-
-public class RegURL {
-    public static String url = "http://192.168.0.110:8084/FinalYearProject/";
-}
