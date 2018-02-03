8c21a378f98198662e29a9951553469f683b41f5
sai3010 <saiprasanth2007@gmail.com>
2018-02-04 01:44:13 +0530

    button done,working on dropdown

--- app/src/main/java/com/example/saipr/final_year_proj/FacultyRegisterActivity.java
+++ app/src/main/java/com/example/saipr/final_year_proj/FacultyRegisterActivity.java
@@ -1,7 +1,12 @@
 package com.example.saipr.final_year_proj;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FacultyRegisterActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class FacultyRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_register);
+        Spinner spinner = (Spinner) findViewById(R.id.qual_drop);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.qual_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+}
+public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+    ...
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+         parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
