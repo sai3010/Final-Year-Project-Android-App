b9713f7493405dd2e4d8ed4ae10b472e07ca06fa
sai3010 <saiprasanth2007@gmail.com>
2018-02-03 17:59:25 +0530

    splash screen and ui designed

---
+++ app/src/main/java/com/example/saipr/final_year_proj/Splashscreen.java
@@ -0,0 +1,29 @@
+package com.example.saipr.final_year_proj;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Thread mythread =new Thread(){
+            @Override
+            public void run()
+            {
+                try {
+                    sleep(3000);
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        mythread.start();
+    }
+}
