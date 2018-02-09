6cadfa34d3596185df421df093a9564cb35c3a9a
sai3010 <saiprasanth2007@gmail.com>
2018-02-09 18:40:09 +0530

    upload code not fully working

--- app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
+++ app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
@@ -6,4 +6,5 @@ package com.example.saipr.final_year_proj;
 
 public class RegURL {
     public static String url = "http://192.168.0.113:8084/FinalYearProjectJSP/";
+    public static String upurl="http://192.168.0.113:8084/Upload/";
 }
