51a1dc6e899a9708d5705a35eab67fa860d2cca8
sai3010 <saiprasanth2007@gmail.com>
2018-02-24 16:13:24 +0530

    added upload for fac

--- app/src/main/java/com/example/saipr/final_year_proj/FacultyDashboardActivity.java
+++ app/src/main/java/com/example/saipr/final_year_proj/FacultyDashboardActivity.java
@@ -1,10 +1,17 @@
 package com.example.saipr.final_year_proj;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,11 +23,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class FacultyDashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final int PICK_FILE_REQUEST = 1;
+    private static final String TAG = FacultyDashboardActivity.class.getSimpleName();
+    private String selectedFilePath="";
+    ProgressDialog dialog;
+    int serverResponseCode = 0;
     String name = "";
     String email="";
     String usn="";
@@ -29,10 +55,13 @@ public class FacultyDashboardActivity extends AppCompatActivity
     TextView usntxt;
     CardView facnotes;
     Intent intent;
+    ImageButton imgbtn=null;
+    Uri selectedFileUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty_dashboard);
+        verifyStoragePermissions(this);
         /*getting usn through intent*/
         name  = getIntent().getExtras().getString("name");
         email = getIntent().getExtras().getString("email");
@@ -95,6 +124,16 @@ public class FacultyDashboardActivity extends AppCompatActivity
         nametxt.setText(name);
         emailtxt.setText(email);
         usntxt.setText(usn);
+        Toast.makeText(this, usn, Toast.LENGTH_SHORT).show();
+        imgbtn= findViewById(R.id.imgbut);
+
+        imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FacultyDashboardActivity.this, "********************", Toast.LENGTH_SHORT).show();
+                showFileChooser();
+            }
+        });
         return true;
     }
 
@@ -130,5 +169,196 @@ public class FacultyDashboardActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        //sets the select file to all types of files
+        intent.setType("*/*");
+        //allows to select data and return it
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        //starts new activity to select file and return data
+        startActivityForResult(Intent.createChooser(intent, "Choose File to Upload.."), PICK_FILE_REQUEST);
+    }
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == PICK_FILE_REQUEST) {
+                if (data == null) {
+                    //no data present
+                    return;
+                }
+
+
+                selectedFileUri = data.getData();
+                selectedFilePath = FilePath.getPath(this, selectedFileUri);
+                Toast.makeText(FacultyDashboardActivity.this, "selectedFilePath=" + selectedFilePath, Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Selected File Path:" + selectedFilePath);
+
+                if (selectedFilePath != null && !selectedFilePath.equals("")) {
+
+
+                    if (selectedFilePath != null) {
+                        dialog = ProgressDialog.show(FacultyDashboardActivity.this, "", "Uploading File...", true);
+                        try {
+                            boolean bool=
+                                    copyFile(selectedFilePath,getExternalCacheDir()+usn+".png");
+                            imgbtn.setImageURI(selectedFileUri);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if(true)
+                        {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+//creating new thread to handle Http Operations
+                                    uploadFile(getExternalCacheDir()+usn+".png");
+                                }
+                            }).start();
+                        }
+                    } else {
+                        Toast.makeText(FacultyDashboardActivity.this, "Please choose a File First", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Cannot upload file to server", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private boolean copyFile(String srcPath,String destPath) throws IOException {
+        boolean bool= false;
+        File srcfile =new File(srcPath);
+        File destfile =new File(destPath);
+//Create a new file and copy the content of old file.
+        InputStream in = new FileInputStream(srcfile);
+        OutputStream out = new FileOutputStream(destfile);
+// Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+        if(destfile.length() == srcfile.length())
+
+        {
+            bool= true;
+        }
+        return bool;
+    }
+
+    public void uploadFile(String sourceFileUri) {
+        String serverResponseMessage="";
+
+        String upLoadServerUri = RegURL.url+"UploadData";
+        String fileName = sourceFileUri;
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 10 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+        if (!sourceFile.isFile()) {
+            Log.e("uploadFile", "Source File Does not exist");
+            // return 0;
+        }
+        try {
+            FileInputStream fileInputStream = new FileInputStream(sourceFile);
+            URL url = new URL(upLoadServerUri);
+            conn = (HttpURLConnection) url.openConnection(); // Open a HTTP  connection to  the URL
+            conn.setDoInput(true); // Allow Inputs
+            conn.setDoOutput(true); // Allow Outputs
+            conn.setUseCaches(false); // Don't use a Cached Copy
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            conn.setRequestProperty("uploaded_file", fileName);
+
+            dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            //dos.writeUTF("Content-Disposition: form-data; name=\"lat\";lat=\""+ lat + "\"" + lineEnd);
+            //  dos.writeUTF(lon);
+            dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""+ fileName + "\"" + lineEnd);
+
+            dos.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available(); // create a buffer of  maximum size
+
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            // read file and write it into form...
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            // send multipart form data necesssary after file data...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // Responses from the server (code and message)
+            serverResponseCode = conn.getResponseCode();
+            serverResponseMessage = conn.getResponseMessage();
+
+
+
+            //close the streams //
+            fileInputStream.close();
+            dos.flush();
+            dos.close();
+
+        } catch (MalformedURLException ex) {
+
+            ex.printStackTrace();
+            Toast.makeText(getApplicationContext(), "MalformedURLException", Toast.LENGTH_SHORT).show();
+            Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Exception : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+        }
+        Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
+        if(serverResponseCode == 200){
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    dialog.cancel();
+                    Toast.makeText(getApplicationContext(), "File Upload Complete.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+        //   return serverResponseCode;
     }
 }
