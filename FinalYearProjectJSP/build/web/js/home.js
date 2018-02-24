b9d5cd9a4ff8ceebc981d5e8944ee0fb245d3076
sunsahar20 <sunsahar14@gmail.com>
2018-02-24 16:14:22 +0530

    radio button fixed

--- FinalYearProjectJSP/build/web/js/home.js
+++ FinalYearProjectJSP/build/web/js/home.js
@@ -17,3 +17,19 @@ function closeNav() {
 $('#mymodal').on('shown.bs.modal', function () {
   $('#addstudent').trigger('focus')
 })
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
