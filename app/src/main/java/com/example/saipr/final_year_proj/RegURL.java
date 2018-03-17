bbc2aaac3c6979572a46d4439a1f6a12dbb569f4
sai3010 <saiprasanth2007@gmail.com>
2018-03-17 18:01:16 +0530

    agg done

--- app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
+++ app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
@@ -5,5 +5,5 @@ package com.example.saipr.final_year_proj;
  */
 
 public interface RegURL {
-    String url = "http://192.168.0.9:8084/FinalYearProjectJSP/";
+    String url = "http://192.168.0.8:8084/FinalYearProjectJSP/";
 }
