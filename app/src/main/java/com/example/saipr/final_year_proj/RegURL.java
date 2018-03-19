ff77f565331d6e29a2b2419a3589a500cdc6190d
sai3010 <saiprasanth2007@gmail.com>
2018-03-19 17:18:17 +0530

    aggregate done

--- app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
+++ app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
@@ -5,6 +5,6 @@ package com.example.saipr.final_year_proj;
  */
 
 public interface RegURL {
-    String url = "http://192.168.0.10:8084/FinalYearProjectJSP/";
-    String durl = "http://192.168.0.10:8084/FinalYearProjectJSP/build/";
+    String url = "http://192.168.0.121:8084/FinalYearProjectJSP/";
+    String durl = "http://192.168.0.121:8084/FinalYearProjectJSP/build/";
 }
