c6834cb23b2eb781360f9212cb8d186a60d00c0b
sai3010 <saiprasanth2007@gmail.com>
2018-03-20 15:34:56 +0530

    fac marks

--- app/src/main/java/com/example/saipr/final_year_proj/Fac_View_Marks.java
+++ app/src/main/java/com/example/saipr/final_year_proj/Fac_View_Marks.java
@@ -1,11 +1,22 @@
 package com.example.saipr.final_year_proj;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +24,8 @@ public class Fac_View_Marks extends AppCompatActivity {
     
     ListView listView;
     Button btnSave;
+    Spinner spin;
+    String usn,res,sem,semval;
     ArrayAdapter<Model> adapter;
     List<Model> list = new ArrayList<Model>();
     @Override
@@ -20,6 +33,23 @@ public class Fac_View_Marks extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fac__view__marks);
         listView = (ListView) findViewById(R.id.my_list);
+        spin=findViewById(R.id.sem_spinner);
+        ArrayAdapter<CharSequence> badapter = ArrayAdapter.createFromResource(this,
+                R.array.sem_lsit, android.R.layout.simple_spinner_item);
+        badapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(badapter);
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                semval = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + "  selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         adapter = new MyAdapter(this,getModel());
         listView.setAdapter(adapter);
     }
@@ -38,5 +68,27 @@ public class Fac_View_Marks extends AppCompatActivity {
         list.add(new Model("1RN14CS012","89"));
 
         return list;
+    }
+    public class fetchdetails extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            try {
+                URL url = new URL(RegURL.url + "FacViewMarks");
+                JSONObject jsn = new JSONObject();
+                jsn.put("usn", usn);
+                res = HttpClientConnection.executeClient(url, jsn);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return res;
+        }
+
     }
 }
