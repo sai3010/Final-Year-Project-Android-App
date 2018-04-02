93a61caee94892705858dd2ae99cba1c54d82c32
sai3010 <saiprasanth2007@gmail.com>
2018-04-02 14:57:33 +0530

    update att servlet

--- FinalYearProjectJSP/src/java/Attendance/UpdateAttendance.java
+++ FinalYearProjectJSP/src/java/Attendance/UpdateAttendance.java
@@ -10,18 +10,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
 
 /**
  *
  * @author saipr
  */
-public class GetTempAttendance extends HttpServlet {
+public class UpdateAttendance extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -33,47 +35,23 @@ public class GetTempAttendance extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, ClassNotFoundException, SQLException {
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-           String sec=request.getParameter("section");
-           String usn=request.getParameter("usn");
-           String branch="";
-           if(usn.contains("CS"))
-           {
-               branch="CSE";
-           }else
-           if(usn.contains("EC"))
-           {
-               branch="ECE";
-           }else
-           if(usn.contains("EE"))
-           {
-               branch="EEE";
-           }else
-            if(usn.contains("ME"))
-           {
-               branch="MECH";
-           }else
-           if(usn.contains("CI"))
-           {
-               branch="CIV";
-           }else
-           if(usn.contains("IS"))
-           {
-               branch="ISE";
-           }else
-           if(usn.contains("EI"))
-           {
-               branch="EIE";
-           }
-            System.out.println("branch = " + branch);
-            System.out.println("sec = " + sec);
             DBQuery db=new DBQuery();
-            ArrayList<String>l=db.getTempAtt(sec.trim(),branch);
-            System.out.println("l = " + l);
-            out.print(l);
+            int j;
+            String li=request.getParameter("list");
+            String sec=request.getParameter("sec");
+            String scode=request.getParameter("scode");
+            System.out.println("scode = " + scode);
+            System.out.println("sec = " + sec);
+            System.out.println("li = " + li);
+            li=li.substring(0,li.length()-1);
+            System.out.println("li = " + li);
+            String []arr=li.split(",");
+            System.out.println("arr = " + arr[0]+arr[1]);
+            for(int i=0;i<arr.length;i++)
+            j=db.updateatt(scode,arr[i],sec);
         }
     }
 
@@ -91,10 +69,10 @@ public class GetTempAttendance extends HttpServlet {
             throws ServletException, IOException {
         try {
             processRequest(request, response);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(GetTempAttendance.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(GetTempAttendance.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UpdateAttendance.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UpdateAttendance.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -111,10 +89,10 @@ public class GetTempAttendance extends HttpServlet {
             throws ServletException, IOException {
         try {
             processRequest(request, response);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(GetTempAttendance.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(GetTempAttendance.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UpdateAttendance.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UpdateAttendance.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
