e094fef6ecfcdb876896d0b6cf31085774c70c4a
sai3010 <saiprasanth2007@gmail.com>
2018-04-25 11:27:43 +0530

    reg url changes

--- app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
+++ app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
@@ -5,6 +5,6 @@ package com.example.saipr.final_year_proj;
  */
 
 public interface RegURL {
-    String url = "http://192.168.0.126:8084/FinalYearProjectJSP/";
+    String url = "http://192.168.0.102:8084/FinalYearProjectJSP/";
    // String durl = "http://192.168.1.10:8084/FinalYearProjectJSP/build/";
 }
