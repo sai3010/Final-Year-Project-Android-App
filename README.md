1a8c6071218a539bbbe1db5be634689b48fd24fd
sai3010 <saiprasanth2007@gmail.com>
2018-06-12 19:07:22 +0530

    removed unwanted files

--- README.md
+++
@@ -1,5 +0,0 @@
-# Final-Year-Project-Android-App
-
-# Branches
-* master - Android App Work Space
-* Android_JSP - Workspace for JSP Servlet.
--- app/src/main/java/com/example/saipr/final_year_proj/RegURL.java
+++
@@ -1,9 +0,0 @@
-package com.example.saipr.final_year_proj;
-
-/**
- * Created by saipr on 2/19/2018.
- */
-
-public interface RegURL {
-    String url = "http://172.20.10.3:8084/FinalYearProjectJSP/";
-}
